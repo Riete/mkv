0ad193842d6f1205a0f5838ecf24ae81bf8b15ea
riete <loli_520@126.com>
2021-11-27 14:17:13 +0800

    optimize

--- kv.go
+++ kv.go
@@ -1,5 +1,9 @@
 package mkv
 
+import (
+	"time"
+)
+
 func Get(k string) (interface{}, error) {
 	return defaultStorage.Get(k)
 }
@@ -14,4 +18,20 @@ func Set(k string, v interface{}) {
 
 func SetNX(k string) bool {
 	return defaultStorage.SetNX(k)
+}
+
+func SetEX(k string, v interface{}, ttl time.Duration) {
+	defaultStorage.SetEX(k, v, ttl)
+}
+
+func TTL(k string) (time.Duration, error) {
+	return defaultStorage.TTL(k)
+}
+
+func IncTTL(k string, ttl time.Duration) error {
+	return defaultStorage.IncTTL(k, ttl)
+}
+
+func DecTTL(k string, ttl time.Duration) error {
+	return defaultStorage.DecTTL(k, ttl)
 }
--- storage.go
+++ storage.go
@@ -8,6 +8,7 @@ import (
 
 var (
 	defaultTTL      = 5 * time.Minute
+	infTTL          = 9999 * time.Minute
 	keyNotExitError = errors.New("key is not exist")
 )
 
@@ -17,7 +18,11 @@ type KVStorage interface {
 	Get(key string) (interface{}, error)
 	Delete(key string)
 	Set(key string, value interface{})
+	SetEX(key string, value interface{}, ttl time.Duration)
 	SetNX(key string) bool
+	TTL(key string) (time.Duration, error)
+	IncTTL(key string, ttl time.Duration) error
+	DecTTL(key string, ttl time.Duration) error
 }
 
 type storage struct {
@@ -27,7 +32,7 @@ type storage struct {
 
 type value struct {
 	data  interface{}
-	ctime time.Time
+	etime time.Time
 }
 
 func (s *storage) clean() {
@@ -35,7 +40,7 @@ func (s *storage) clean() {
 		time.Sleep(s.ttl)
 		s.storage.Range(func(k, v interface{}) bool {
 			d := v.(value)
-			if time.Now().Second()-d.ctime.Second() > int(s.ttl.Seconds()) {
+			if time.Now().After(d.etime) {
 				s.storage.Delete(k)
 			}
 			return true
@@ -43,13 +48,25 @@ func (s *storage) clean() {
 	}
 }
 
-func (s *storage) Get(k string) (interface{}, error) {
+func (s *storage) get(k string) (*value, error) {
 	v, ok := s.storage.Load(k)
 	if !ok {
-		return "", keyNotExitError
+		return nil, keyNotExitError
 	}
-	d := v.(value)
-	return d.data, nil
+	d := v.(*value)
+	if time.Now().After(d.etime) {
+		s.storage.Delete(k)
+		return nil, keyNotExitError
+	}
+	return d, nil
+}
+
+func (s *storage) Get(k string) (interface{}, error) {
+	if v, err := s.get(k); err != nil {
+		return nil, err
+	} else {
+		return v.data, nil
+	}
 }
 
 func (s *storage) Delete(k string) {
@@ -57,14 +74,45 @@ func (s *storage) Delete(k string) {
 }
 
 func (s *storage) Set(k string, v interface{}) {
-	d := value{data: v, ctime: time.Now()}
+	d := &value{data: v, etime: time.Now().Add(defaultTTL)}
+	s.storage.Store(k, d)
+}
+
+func (s *storage) SetEX(k string, v interface{}, ttl time.Duration) {
+	d := &value{data: v, etime: time.Now().Add(ttl)}
 	s.storage.Store(k, d)
 }
 
 func (s *storage) SetNX(k string) bool {
-	d := value{data: "", ctime: time.Now()}
+	d := &value{data: "", etime: time.Now().Add(infTTL)}
 	_, loaded := s.storage.LoadOrStore(k, d)
 	return !loaded
+}
+
+func (s *storage) TTL(k string) (time.Duration, error) {
+	if v, err := s.get(k); err != nil {
+		return 0, err
+	} else {
+		return v.etime.Sub(time.Now()), nil
+	}
+}
+
+func (s *storage) adjTTL(k string, ttl time.Duration) error {
+	if v, err := s.get(k); err != nil {
+		return err
+	} else {
+		v.etime = v.etime.Add(ttl)
+		s.storage.Store(k, v)
+		return nil
+	}
+}
+
+func (s *storage) IncTTL(k string, ttl time.Duration) error {
+	return s.adjTTL(k, ttl)
+}
+
+func (s *storage) DecTTL(k string, ttl time.Duration) error {
+	return s.adjTTL(k, -ttl)
 }
 
 func NewKVStorage(ttl time.Duration) KVStorage {
